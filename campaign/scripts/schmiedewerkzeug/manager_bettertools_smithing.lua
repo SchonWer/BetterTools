eae23465741ab936bcc685090df1f70b67c5a631
JH_HSAalen <julian.haenle@studmail.htw-aalen.de>
2026-05-29 15:53:50 +0200

    Schmiedeausrüstung, Text abgeändert

--- campaign/scripts/schmiedewerkzeug/manager_bettertools_smithing.lua
+++ campaign/scripts/schmiedewerkzeug/manager_bettertools_smithing.lua
@@ -1,37 +1,27 @@
 function getToolText()
 	return [[<formattedtext>
 <p><i>Genutztes Attribut: Stärke</i></p>
-<p>Übung mit Schmiedewerkzeug ermöglicht es dir, Waffen und Rüstungen selbst herzustellen und sogar magische Gegenstände zu kreieren, solange sie aus Metall bestehen.</p>
-<p>Schmiedewerkzeug enthält Hämmer, Zangen, Kohle, Lappen, einen 20 Pfund schweren Amboss und einen Wetzstein. Es wiegt 28 Pfund und kostet 20 Gold.</p>
+<p>Übung mit Schmiedewerkzeug ermöglicht es dir Waffen und Rüstungen selbst herzustellen und sogar Magische Gegenstände zu kreieren, solange sie aus Metall bestehen. Schmiedewerkzeug enthält Hämmer, Zangen, Kohle, Lappen, einen 20 Pfund schweren Amboss und einen Wetzstein. Es wiegt 28 Pfund und kostet 20 Gold.</p>
 <h>Fertigkeiten</h>
 <p>Folgende Beispiele zeigen auf, inwiefern andere Fertigkeiten von der Übung in Schmiedewerkzeug profitieren könnten. Es handelt sich dabei lediglich um Vorschläge; sei kreativ!</p>
-<p><b>Geschichte.</b> Anhand von metallenen Gegenständen und ihrem Aufbau bzw. der Art und Weise, wie sie hergestellt wurden, kannst du Annahmen über ihr Alter und ihre Herkunft tätigen.</p>
-<p><b>Heilkunde.</b> Während deiner Arbeit hast du dich schon mehr als einmal verbrannt oder anderweitig verletzt. Dadurch fällt es dir leichter, solche Verletzungen zu erkennen und oberflächlich zu behandeln.</p>
+<p><b>Geschichte.</b> Anhand von metallenen Gegenständen und ihrem Aufbau bzw. der Art und Weise wie sie hergestellt wurden, kannst du Annahmen über ihr Alter und ihre Herkunft tätigen.</p>
+<p><b>Heilkunde.</b> Während deiner Arbeit hast du dich schon mehr als einmal verbrannt oder anderweitig verletzt. Dadurch fällt es dir leichter solche Verletzungen zu erkennen und oberflächlich zu behandeln.</p>
 <p><b>Motiv Erkennen.</b> Durch deine Arbeit hattest du mit einigen Kriegern, Soldaten und Söldnern zu tun. Du hast ein Gespür dafür, ob dieser Grobian sich nur so gibt oder tatsächlich was auf dem Kasten hat, wie militärisch jemand veranlagt ist und dergleichen.</p>
 <h>Schmieden</h>
-<p>Du kannst grundsätzlich alle Waffen und Rüstungen herstellen, die aus Metall gefertigt werden, ebenso wie andere Objekte. Lederrüstungen oder Gegenstände aus anderen Materialien wie Holz oder Stein liegen jedoch außerhalb deiner Handwerkskunst. Außerdem ist es dir nicht möglich, filigrane Schmuckstücke wie Ringe, Ketten oder dergleichen anzufertigen. Dazu ist Übung mit Juwelierwerkzeug erforderlich.</p>
-<p>Unabhängig davon, ob du ein gewöhnliches oder besonderes Projekt schmiedest, musst du am Ende des Schmiedeprozesses einen Stärke (Schmiedewerkzeug)-Wurf ablegen, um zu sehen, ob du den Gegenstand erfolgreich herstellst. Solltest du den Wurf nicht schaffen, kannst du ein Viertel der benötigten Zeitdauer aufwenden, um den Gegenstand auszubessern und den Wurf zu wiederholen. Dieser Prozess kann so oft wiederholt werden wie nötig.</p>
+<p>Du kannst grundsätzlich alle Waffen und Rüstungen herstellen, die aus Metall gefertigt werden, ebenso wie andere Objekte. Lederrüstungen oder Gegenstände aus anderen Materialien wie Holz, Stein etc. liegen jedoch außerhalb deiner Handwerkskunst. Außerdem ist es dir auch nicht möglich intrigante Schmuckstücke wie Ringe, Ketten oder dergleichen anzufertigen. Dazu ist Übung mit Juwelierwerkzeug erforderlich.</p>
+<p>Unabhängig davon, ob du ein gewöhnliches oder besonderes Projekt schmiedest, musst du am Ende des Schmiedeprozesses einen Stärke (Schmiedewerkzeug)-Wurf ablegen, um zu sehen, ob du den Gegenstand erfolgreich herstellst oder nicht. Solltest du den Wurf nicht schaffen, kannst du einen Viertel der benötigten Zeitdauer aufwenden, um den Gegenstand auszubessern und den Wurf zu wiederholen. Dieser Prozess kann so oft wiederholt werden wie nötig.</p>
 <h>Gewöhnliche und Besondere Projekte</h>
-<p>Um den Schmiedeprozess zu vereinfachen, wird lediglich zwischen gewöhnlichen und besonderen Projekten unterschieden, wenn es um ihre Herstellung geht.</p>
-<p>Gewöhnliche Projekte beinhalten unter anderem sämtliche Kriegs- und einfachen Waffen sowie Rüstungen. Gegenstände wie Zangen, Töpfe und ähnliche Objekte fallen ebenfalls darunter.</p>
-<p>Besondere Projekte beschreiben hingegen magische Gegenstände, Waffen und Rüstungen. Unabhängig von der Kategorie kostet die Herstellung eines Gegenstands die Hälfte seines normalen Kaufpreises.</p>
+<p>Gewöhnliche Projekte beinhalten sämtliche nichtmagischen Gegenstände.</p>
+<p>Besondere Projekte beschreiben hingegen Magische Gegenstände, Waffen und Rüstungen.</p>
+<p>Unabhängig von der Kategorie kostet die Herstellung eines Gegenstands die Hälfte seines normalen Kaufpreises.</p>
 <h>Gewöhnliche Projekte</h>
-<p>Gewöhnliche Projekte können unterwegs, rein mithilfe deines Schmiedewerkzeugs und einer geeigneten Wärmequelle, beispielsweise einem Lagerfeuer, geschmiedet werden. Außerdem ist es nicht nötig, dass du die verwendeten Rohmaterialien bei dir trägst.</p>
+<p>Gewöhnliche Projekte können unterwegs, rein mithilfe deines Schmiedewerkzeugs und einer geeigneten Wärmequelle (bspw. einem Lagerfeuer) geschmiedet werden.</p>
 <h>Besondere Projekte</h>
-<p>Besondere Projekte können nicht unterwegs hergestellt werden und benötigen eine stationäre Schmiede. Diese muss nicht zwingend dir selbst gehören; gegen einen passenden Betrag darfst du sicherlich auch in fremden Schmieden arbeiten.</p>
+<p>Besondere Projekte können nicht unterwegs hergestellt werden und benötigen eine stationäre Schmiede.</p>
 <p>Das Schmieden besonderer Projekte ist immer mit dem Auftreiben der richtigen Materialien verbunden. Willst du etwa eine Adamantinrüstung schmieden, so brauchst du zunächst die nötige Menge an Adamantin. Eine Blutaxt hingegen könnte die Zähne eines Vampirs für ihre Herstellung brauchen.</p>
-<p>Die meiste Zeit, die für ein besonderes Projekt aufgewendet wird, betrifft vermutlich die Materialsuche. Das eigentliche Schmieden selbst dauert jedoch etwas länger als bei gewöhnlichen Projekten.</p>
 <h>Zeitdauer und Schwierigkeitsgrad</h>
 <h>Gewöhnliche Projekte</h>
-<table>
-	<tr><td><b>Art des Gegenstandes</b></td><td><b>Arbeitszeit in Stunden</b></td><td><b>SG</b></td></tr>
-	<tr><td>Einfache Waffe</td><td>2</td><td>5</td></tr>
-	<tr><td>Kriegswaffe / Schild</td><td>4</td><td>10</td></tr>
-	<tr><td>Leichte Rüstung</td><td>4</td><td>5</td></tr>
-	<tr><td>Mittlere Rüstung</td><td>8</td><td>10</td></tr>
-	<tr><td>Schwere Rüstung</td><td>16</td><td>12</td></tr>
-</table>
-<p><i>Die im Spielerhandbuch enthaltenen leichten Rüstungen bestehen nicht aus Metall und können daher nicht geschmiedet werden. Die Angabe dient für Homebrew-Content.</i></p>
+<p>Gewöhnliche Gegenstände stellst du während einer Langen Rast her, wobei du deren positive Effekte erhältst. Der SG für die Herstellung des jeweiligen Gegenstands liegt bei 10.</p>
 <h>Besondere Projekte</h>
 <table>
 	<tr><td><b>Seltenheit des Gegenstandes</b></td><td><b>Arbeitszeit in Stunden</b></td><td><b>SG</b></td></tr>
@@ -42,6 +32,6 @@ function getToolText()
 	<tr><td>Legendär</td><td>64</td><td>25</td></tr>
 </table>
 <h>Schmieden von Munition</h>
-<p>Pfeile und Bolzen sind vergleichsweise simpel zu kreieren. Du kannst pro Tag so viele Stücke Munition herstellen, wie deinem Stärkemodifikator + deinem Übungsbonus entspricht. Es wird angenommen, dass du diese Aktivität automatisch ausführst, wenn du ein wenig Freizeit hast, anstatt aktiv eine genaue Zeitspanne daran zu arbeiten.</p>
+<p>Pfeile und Bolzen sind vergleichsweise simpel zu kreieren. Du kannst pro Tag so viel Stücke Munition herstellen wie es deinem Stärkemodifikator + deinem Übungsbonus entspricht. Es wird angenommen, dass du diese Aktivität automatisch ausführst, wenn du ein wenig Freizeit hast, anstatt aktiv eine genaue Zeitspanne daran zu arbeiten.</p>
 </formattedtext>]];
 end
